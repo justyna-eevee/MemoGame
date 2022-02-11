3e68a909e596c2c7afabae0e159c733a3915711d
justyna-eevee <justyna.zaczyk@onet.pl>
2022-02-11 15:09:36 +0100

    heroku settings

--- database_common.py
+++ database_common.py
@@ -28,8 +28,10 @@ def get_connection_string():
 
 def open_database():
     try:
-        connection_string = get_connection_string()
+        connection_string = os.environ.get('DATABASE_URL')
         connection = psycopg2.connect(connection_string)
+        # connection_string = get_connection_string()
+        # connection = psycopg2.connect(connection_string)
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
